6d4721251bd74835fae39321729b20b420da6d7b
Maxon Tayebwa <maxtayebwa@gmail.com>
2021-07-11 19:23:43 +0300

    Add a service

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
-export class CoffeesController {}
+export class CoffeesController {
+  constructor(private coffeeService: CoffeesService) {}
+
+  @Get()
+  public findAll() {
+    return this.coffeeService.findAll();
+  }
+
+  @Get(':id')
+  public findOne(@Param('id') id: string) {
+    return this.coffeeService.findOne(id);
+  }
+
+  @Post()
+  public create(@Body() body) {
+    return this.coffeeService.create(body);
+  }
+
+  @Patch(':id')
+  public update(@Param('id') id: string, @Body() body: any) {
+    return this.coffeeService.update(id, body);
+  }
+
+  @Delete(':id')
+  public remove(@Param('id') id: string) {
+    return this.coffeeService.remove(id);
+  }
+}
